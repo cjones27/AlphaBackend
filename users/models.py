8ec5f872fb721d7da7b34c83abcb137c46f540cd
Cristobal Jones <cjones2@uc.cl>
2021-04-26 19:51:26 -0400

    users migration

---
+++ users/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+# from django.contrib.auth.models import User
+
+# Create your models here.
+
+# constants
+name_length = 200
+password_length = 128
+
+# Users model
+class User(models.Model):
+    first_name = models.CharField(max_length=name_length)
+    last_name = models.CharField(max_length=name_length)
+    user_type = models.IntegerField(default=0)
+    date_of_birth = models.DateField()
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.TextField(default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# class UserManager_(models.Manager):
+#     pass
+
+
+# class ThreadManager(models.Manager):
+
+#     obj = ThreadModel.objects
+
+#     @staticmethod
+#     def create_new_thread(subject, board_id):
+#         curr_board = BoardModel.objects.get(board_id = board_id)
+#         thread = ThreadManager.obj.create(board= curr_board, thread_name=subject)
+#         thread.save()
+#         return thread
+
+#     @staticmethod
+#     def get_all_messages_in_thread(thread):
+#         messages = MessageModel.objects.filter(thread = thread)
+#         return messages
+
+# @router.post("/register", response=UserOutPrivate, auth=None)
+# def create_user(request, user: UserIn):
+#     user_model = User(**user.dict())
+#     user_model.set_password(user.password)
+#     user_model.save()
+
+#     # return value includes api_key
+#     return user_model
